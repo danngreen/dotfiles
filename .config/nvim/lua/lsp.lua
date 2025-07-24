c3a902fc88f6269349b9155151daf16bea646abb
Dan Green <danngreen1@gmail.com>
2025-07-23 21:55:19 -0700

    add lsp diagnostic virttext

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,3 +1,17 @@
 vim.lsp.enable("clangd")
 vim.lsp.enable("pyright")
 -- vim.lsp.enable("python-language-server")
+
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	update_in_insert = false,
+	underline = true,
+	severity_sort = true,
+	float = {
+		border = 'rounded',
+		source = 'always',
+		header = '',
+		prefix = '',
+	},
+})
