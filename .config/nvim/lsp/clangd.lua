ae0f1837b01cb2b77b95d782d16fa7fd6bfbd34f
Dan Green <danngreen1@gmail.com>
2025-07-23 21:55:31 -0700

    Re-arrange and simplify clangd config

--- .config/nvim/lsp/clangd.lua
+++ .config/nvim/lsp/clangd.lua
@@ -1,19 +1,18 @@
-local clangbin = "clangd"
+local clangdbin = "clangd"
+-- clangdbin = "/Users/dann/bin/clangd_18.1.3/bin/clangd",
+-- clangdbin = "/Users/dann/bin/clangd_17.0.3/bin/clangd",
+-- clangdbin = "/Users/dann/bin/clangd_16.0.2/bin/clangd",
+-- clangdbin = "/Users/dann/bin/clangd_snapshot_20240714/bin/clangd",
+-- clangdbin = "/Users/dann/bin/clangd_snapshot_20240721/bin/clangd",
 
--- Not needed, but might be useful. Why does lua filter out the 4m?
+-- Not needed, but might be useful
 -- if string.find(vim.fn.getcwd(), "vcv/4ms") then
--- 	clangbin = "/Users/dann/bin/clangd_16.0.2/bin/clangd"
+-- 	clangdbin = "/Users/dann/bin/clangd_16.0.2/bin/clangd"
 -- end
 
 vim.lsp.config('clangd', {
 	cmd = {
-		clangbin,
-		-- "clangd",
-		-- "/Users/dann/bin/clangd_18.1.3/bin/clangd",
-		-- "/Users/dann/bin/clangd_17.0.3/bin/clangd",
-		-- "/Users/dann/bin/clangd_16.0.2/bin/clangd",
-		-- "/Users/dann/bin/clangd_snapshot_20240714/bin/clangd",
-		-- "/Users/dann/bin/clangd_snapshot_20240721/bin/clangd",
+		clangdbin,
 		"--background-index",
 		"--log=error",
 		-- "--log=verbose",
@@ -23,17 +22,12 @@ vim.lsp.config('clangd', {
 		"--clang-tidy",
 		"--header-insertion-decorators",
 		"--completion-style=bundled",
-		"--query-driver=/usr/local/bin/arm-none-eabi-g*",
-		"--query-driver=/Users/**/4ms/stm32/*-arm-none-eabi*/bin/arm-none-eabi-*",
-		"--query-driver=/Volumes/Studio/bin/*-arm-none-eabi*/bin/arm-none-eabi-*",
-		"--query-driver=/Users/**/bin/*-arm-none-eabi*/bin/arm-none-eabi-*",
+		"--query-driver=**/bin/arm-none-eabi-g*",
 		"--query-driver=/usr/bin/g*",
-		"--query-driver=/usr/local/opt/llvm/bin/clang*",
-		"--query-driver=/Users/dann/bin/arm-gnu-toolchain-12.3.rel1-darwin-arm64-arm-none-eabi/bin/arm-none-eabi-*",
-		"--query-driver=/Users/dann/bin/arm-gnu-toolchain-13.2.Rel1-darwin-arm64-aarch64-none-elf/bin/aarch64-none-elf-*",
-		"--query-driver=/Users/dann/Library/Arduino15/packages/arduino/tools/avr-gcc/7.3.0-atmel3.6.1-arduino7/bin/avr-*",
+		"--query-driver=**/bin/aarch64-none-elf-*",
+		"--query-driver=**/bin/avr-*",
 		"--query-driver=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/c++",
-		"--query-driver=/opt/homebrew/opt/llvm/bin/clang",
+		"--query-driver=**/llvm/bin/clang",
 		"--pch-storage=memory",
 		"--enable-config"
 	},
@@ -55,6 +49,13 @@ vim.lsp.config('clangd', {
 		end
 		client.config.allow_incremental_sync = true
 		client.config.debounce_text_changes = 100
+
+		-- Does not work to modify clangdbin here:
+		-- if string.find(client.root_dir, "vcv/4ms") then
+		-- 	clangdbin = "/Users/dann/bin/clangd_16.0.2/bin/clangd"
+		-- end
+
+
 		-- client.server_capabilities.semanticTokensProvider = nil
 	end,
 })
