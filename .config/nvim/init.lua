520d12c8ec3195bb8ce59630a9d85f15f6861b8d
Dan Green <danngreen1@gmail.com>
2025-07-23 23:25:20 -0700

    Format on save

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -77,6 +77,23 @@ vim.api.nvim_create_autocmd({"BufWritePost"}, {
   desc = "Immediately apply changes on save"
 })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp.attach', {}),
+  callback = function(args)
+	local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+	if not client:supports_method('textDocument/willSaveWaitUntil')
+		and client:supports_method('textDocument/formatting') then
+	  vim.api.nvim_create_autocmd('BufWritePre', {
+		group = vim.api.nvim_create_augroup('lsp.attach', {clear=false}),
+		buffer = args.buf,
+		callback = function()
+		  vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+		end,
+	  })
+	end
+  end,
+})
+
 
 vim.cmd [[set exrc]]
 
