66e096e7fd7cff737b8237c15002a16c976537b6
Dan Green <danngreen1@gmail.com>
2025-09-17 15:34:11 -0700

    clangd logic calls nvim-lspconfig to run its on_attach

--- .config/nvim/lsp/clangd.lua
+++ .config/nvim/lsp/clangd.lua
@@ -39,10 +39,24 @@ vim.lsp.config('clangd', {
 		'.git',
 	},
 	on_attach = function(client, bufnr)
+		-- This is a complicated way to call the on_attach function in nvim-lspconfig/lsp/clangd.lua
+		-- because that file defines LspClangdSwitchSourceHeader, which we want to map to a key.
+
+		-- First, get paths to all files named lsp/clangd.lua in our runtime path
 		local files = vim.api.nvim_get_runtime_file("lsp/clangd.lua", true)
-		-- TODO: try all files but skip the one that's this file
-		if #files > 0 then
-			local upstream_attach = dofile(files[#files]).on_attach
+
+		-- Find just the one that's in nvim-lspconfig
+		local upstream_file = nil
+		for _, f in ipairs(files) do
+			if f:match("nvim%-lspconfig") then
+				upstream_file = f
+				break
+			end
+		end
+
+		-- If we found it, then call the file (dofile) and call the on_attach that it returns
+		if upstream_file then
+			local upstream_attach = dofile(upstream_file).on_attach
 			if upstream_attach then
 				upstream_attach(client, bufnr)
 				vim.keymap.set("n", "<M-h>", "<cmd>LspClangdSwitchSourceHeader<CR>", { buffer = bufnr })
