a6dd5a79306f907bd0a6d2e89d44f4e9c1db2ab2
Dan Green <danngreen1@gmail.com>
2026-03-13 12:01:22 -0700

    Do not format cmake files

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -28,12 +28,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
 		-- Format on save
-		if client:supports_method('textDocument/formatting') then
-			vim.api.nvim_create_autocmd('BufWritePre', {
-				group = vim.api.nvim_create_augroup('lsp.attach', { clear = false }),
-				buffer = args.buf,
-				callback = function() vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 }) end,
-			})
+		if client.name ~= "cmake" then
+			if client:supports_method('textDocument/formatting') then
+				vim.api.nvim_create_autocmd('BufWritePre', {
+					group = vim.api.nvim_create_augroup('lsp.attach', { clear = false }),
+					buffer = args.buf,
+					callback = function() vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 }) end,
+				})
+			end
 		end
 	end,
 })
