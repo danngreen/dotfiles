dcdbd6e7df01c311f7a23c34fbcaca6a130e2d95
Dan Green <danngreen1@gmail.com>
2026-04-02 09:01:01 -0700

    Remove lua lsp hint for vim.lsp

--- .config/nvim/lua/types.lua
+++ .config/nvim/lua/types.lua
@@ -7,7 +7,7 @@ vim = require("vim.uri")
 vim = require("vim.inspect")
 
 -- let sumneko know where the sources are for the global vim runtime
-vim.lsp = require("vim.lsp")
+-- vim.lsp = require("vim.lsp")
 vim.treesitter = require("vim.treesitter")
 vim.highlight = require("vim.highlight")
 
